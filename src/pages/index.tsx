3881a5e0f483a8b5b02ce215b540c8d4fb934c03
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-13 15:41:46 +0530

    something

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import { NextPage } from 'next'
-import { Box, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Heading, Input, Text, VStack } from '@chakra-ui/react'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+	
+	const [documentUrl, setDocumentUrl] = useState<string>('')
+
 	return(
 		<Box px={8} py={8}>
 			<Heading>some ai app</Heading>
 			<VStack>
 				<Text>Enter the document URL</Text>
+				<Input value={documentUrl} onChange={({ target }) => setDocumentUrl(target.value)}/>
+				<Button>Create BOT</Button>
 			</VStack>
 		</Box>
 	)
