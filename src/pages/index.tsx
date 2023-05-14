4d60d6eabfad73997ba9bc4d0377cb0a4459267a
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-14 09:05:15 +0530

    v0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,24 +7,32 @@ const Home: NextPage = () => {
 	const [documentUrl, setDocumentUrl] = useState<string>('')
 	const [prompt, setPrompt] = useState('')
 	const [reply, setReply] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
 
-	const queryDocument = async () => {
+	const queryDocument = async (prompt: string) => {
+		setIsLoading(true)
+		const body = JSON.stringify({ prompt })
 		const res = await fetch(`http://localhost:8080/api/query`, {
 			method: 'POST',
-			body: JSON.stringify({ prompt })
+			body,
+			headers: {
+				"Content-Type": "application/json"
+			}
 		})
 		const answer = await res.json()
+		console.log(answer)
 		setReply(answer.response.text)
+		setIsLoading(false)
 	}
 
 	return(
 		<Box px={8} py={8}>
-			<Heading>some ai app</Heading>
+			<Heading>docusearch.ai</Heading>
 			<VStack>
 				<Text>Enter the document URL</Text>
 				<Input value={documentUrl} onChange={({ target }) => setDocumentUrl(target.value)}/>
 				<Input value={prompt} onChange={({ target }) => setPrompt(target.value)}/>
-				<Button onClick={queryDocument} disabled={!documentUrl}>Create BOT</Button>
+				<Button isLoading={isLoading} onClick={() => queryDocument(prompt)} disabled={!prompt}>Query</Button>
 				{
 					reply && <Text>{reply}</Text>
 				}
