0ecd83543999af4714645e082b4dd42e1da0180c
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-14 08:42:29 +0530

    honestly dunno what i did

--- src/pages/api/load.ts
+++ src/pages/api/load.ts
@@ -5,5 +5,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const loadDocuments = async (documentUrl: string) => {
 	const gitbookLoader = new GitbookLoader(documentUrl, { shouldLoadAllPaths: true })
 	const docs = await gitbookLoader.load()
-	console.log(docs)
+	return docs
 }
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	if(req.method == 'POST'){
+		try {
+			const { url } = await req.body
+			const docs = await loadDocuments(url)
+			res.status(200).json({ message: "Successfully Created DB" })
+		}
+		catch (err){
+			res.status(400).json({ err })
+		}
+	}
+}
+
+export default handler 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,16 @@ import { useState } from 'react'
 const Home: NextPage = () => {
 	
 	const [documentUrl, setDocumentUrl] = useState<string>('')
+	const [prompt, setPrompt] = useState('')
+	const [reply, setReply] = useState('')
 
-	
-	const loadDocuments = async () => {
-		
+	const queryDocument = async () => {
+		const res = await fetch(`http://localhost:8080/api/query`, {
+			method: 'POST',
+			body: JSON.stringify({ prompt })
+		})
+		const answer = await res.json()
+		setReply(answer.response.text)
 	}
 
 	return(
@@ -17,7 +23,11 @@ const Home: NextPage = () => {
 			<VStack>
 				<Text>Enter the document URL</Text>
 				<Input value={documentUrl} onChange={({ target }) => setDocumentUrl(target.value)}/>
-				<Button onClick={loadDocuments}>Create BOT</Button>
+				<Input value={prompt} onChange={({ target }) => setPrompt(target.value)}/>
+				<Button onClick={queryDocument} disabled={!documentUrl}>Create BOT</Button>
+				{
+					reply && <Text>{reply}</Text>
+				}
 			</VStack>
 		</Box>
 	)
