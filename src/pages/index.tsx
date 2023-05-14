7e5b424627e05e2866fbabaa8b81426ed5a5ab2a
Shlok D. Mange <shlok_mange@protonmail.com>
2023-05-14 10:05:35 +0530

    fixed ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,33 @@ import { useState } from 'react'
 const Home: NextPage = () => {
 	
 	const [documentUrl, setDocumentUrl] = useState<string>('')
+	const [docResponse, setDocResponse] = useState<string>('')
+
 	const [prompt, setPrompt] = useState('')
 	const [reply, setReply] = useState('')
+	
 	const [documentIsLoading, setDocumentIsLoading] = useState(false)
 	const [replyIsLoading, setReplyIsLoading] = useState(false)
 	
+	const loadDocument = async () => {
+		setDocumentIsLoading(true)
+		const body = JSON.stringify({ url: documentUrl })
+		const res = await fetch(`http://localhost:8080/api/load`, {
+			method: 'POST',
+			body,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		if(res.status === 200){
+			setDocResponse('Successfully Parsed the Document!')
+		}
+		else {
+			setDocResponse('Error loading document. Please try again')
+		}
+		setDocumentIsLoading(false)
+	}
+
 	const queryDocument = async (prompt: string) => {
 		setReplyIsLoading(true)
 		const body = JSON.stringify({ prompt })
@@ -33,9 +55,12 @@ const Home: NextPage = () => {
 				<Stack w={'50%'}>
 					<Text>Enter the document URL</Text>
 					<Input value={documentUrl} onChange={({ target }) => setDocumentUrl(target.value)}/>
-					<Button isLoading={documentIsLoading} onClick={() => queryDocument(prompt)} disabled={!prompt}>
+					<Button isLoading={documentIsLoading} onClick={loadDocument} disabled={!prompt}>
 						Add Document
 					</Button>
+					{
+						docResponse && <Text>{docResponse}</Text>
+					}
 				</Stack>
 				<Stack w={'50%'}>
 					<Text>Enter the Prompt</Text>
